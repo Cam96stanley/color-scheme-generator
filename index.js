031dea48a11d9ce4c55fcb1fec4ffeecb784ec42
cam96stanley <cam96stanley@gmail.com>
2024-10-18 13:59:09 -0400

    Added js, updated css and html
    Created the fetch and DOM manipulation to display
    the colors. Added css to display the colors
    correctly.

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-fetch("https://www.thecolorapi.com/scheme?hex=000000&mode=analogic")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+const seed = document.getElementById("color-seed");
+const schemeType = document.getElementById("scheme-type");
+const colorContainer = document.getElementById("color-container");
+
+document.getElementById("color-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const seedColor = seed.value.replace(/^#/, "");
+  const colorScheme = schemeType.value;
+  const colorCount = 5;
+  let html = "";
+
+  fetch(
+    `https://www.thecolorapi.com/scheme?hex=${seedColor}&mode=${colorScheme}&count=${colorCount}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      let colors = [];
+      for (let i = 0; i < data.colors.length; i++) {
+        colors.push(data.colors[i].hex.value);
+      }
+      console.log(colors);
+      colors.forEach(function (color) {
+        html += `
+        <div class="color-item">
+          <div class="color-div" style="background-color:${color}"></divclass=style=>
+          <p class="color-hex">${color}</p>
+        </div>`;
+      });
+      colorContainer.innerHTML = html;
+    });
+});
